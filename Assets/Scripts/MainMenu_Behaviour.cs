498b0714201ff1771d388afccb51165836dd49ea
bfejwndbdu <officiallyrichard@gmail.com>
2025-08-12 00:31:07 +0800

    Main Menu Sound effects,BGM, Buttons
    Still missing background vid, need character models

--- Assets/Scripts/MainMenu_Behaviour.cs
+++ Assets/Scripts/MainMenu_Behaviour.cs
@@ -1,10 +1,16 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu_Behaviour : MonoBehaviour
 {
-    public void PlayGame()
+    public void StartGame()
     {
+        StartCoroutine(PlayGame());
+    }
+    IEnumerator PlayGame()
+    {
+        yield return new WaitForSeconds(1);
         SceneManager.LoadSceneAsync(1);
     }
     public void QuitGame()
